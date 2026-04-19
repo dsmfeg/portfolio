e0df7ba55c44bc4ca0600f9cc68414b52621c421
dsmfeg <dsmfeg696@gmail.com>
2026-04-18 22:02:29 -0700

    dsmfeg.github.io on links
    stupid fucking idiot, stupid idiot

--- global.js
+++ global.js
@@ -28,7 +28,7 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                  // Local server
-    : "/website/";         // GitHub Pages repo name
+    : "dsmfeg.github.io/";         // GitHub Pages repo name
 
 for (let p of pages) {
     let url = p.url;

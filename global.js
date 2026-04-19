bc224a01739e1af2a9df00848b8029167a198038
dsmfeg <dsmfeg696@gmail.com>
2026-04-18 21:53:01 -0700

    lab 03
    lab03ah

---
+++ global.js
@@ -0,0 +1,74 @@
+// console.log('IT’S ALIVE!');
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// let navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink.classList.add('current');
+// }
+
+// Navigation element
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'Resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact Me' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"                  // Local server
+    : "/website/";         // GitHub Pages repo name
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    nav.append(a);
+}
+
+// Light and Dark Mode Selection
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="light dark">Automatic</option>
+		</select>
+	</label>`,
+);
+
+let select = document.querySelector('.color-scheme select')
+
+if ('colorScheme' in localStorage) {
+  const saved = localStorage.colorScheme;
+
+  document.documentElement.style.setProperty('color-scheme', saved);
+  select.value = saved;
+}
+
+select.addEventListener('input', function (event) {
+  const value = event.target.value;
+
+  document.documentElement.style.setProperty('color-scheme', value);
+  localStorage.colorScheme = value;
+});

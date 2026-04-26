79ac358535fd76635a3c4b3544d2ebd3b5c5f0b9
dsmfeg <dsmfeg696@gmail.com>
2026-04-25 21:52:39 -0700

    lab 04
    maybe prolly not final we'll see

--- global.js
+++ global.js
@@ -72,3 +72,51 @@ select.addEventListener('input', function (event) {
   document.documentElement.style.setProperty('color-scheme', value);
   localStorage.colorScheme = value;
 });
+
+
+// Projects JSON
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+// export function renderProjects(project, containerElement) {
+//   containerElement.innerHTML = '';
+//   const article = document.createElement('article');
+//   article.innerHTML = `
+//     <h3>${project.title}</h3>
+//     <img src="${project.image}" alt="${project.title}">
+//     <p>${project.description}</p>
+//   `;
+//   containerElement.appendChild(article);
+// }
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  projects.forEach(project => {
+    const article = document.createElement('article');
+    const heading = document.createElement(headingLevel);
+    heading.textContent = project.title;
+
+    article.innerHTML = `
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    article.prepend(heading);
+    containerElement.appendChild(article);
+  });
+}
+
+//Github API
+export async function fetchGithubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
